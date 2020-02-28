e99f2a6341d2d808ced201ccb82a0b53bd835a20
charles johnston <charles.ward.johnston@gmail.com>
2020-02-27 23:22:09 -0500

    Initial Commit

---
+++ BarbellWeightCalculator/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BarbellWeightCalculator
+//
+//  Created by charles johnston on 2/27/20.
+//  Copyright © 2020 Zin Studio. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
